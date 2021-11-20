ae4e9a973e75ba05e15fe731a186d12d79b6049a
Artem Aksentenko MAC <html@centrsvet.ru>
2021-11-20 11:04:22 +0300

    tickets

--- problems/003-ticket.js
+++ problems/003-ticket.js
@@ -14,7 +14,23 @@
  * @returns {boolean}
  */
 function checkTicket(number) {
-    return undefined;
+    // const nums = number.split("").map(Number);
+    // let left = 0;
+    // let right = 0;
+
+    // nums.forEach((element, idx) => {
+    //     idx < 3 ? (left += element) : (right += element);
+    // });
+
+    const tickets = number.split("").reduce(
+        (acc, item, idx) => {
+            idx < 3 ? (acc.left += +item) : (acc.right += +item);
+            return acc;
+        },
+        { left: 0, right: 0 }
+    );
+
+    return tickets.left === tickets.right;
 }
 
 module.exports = checkTicket;
