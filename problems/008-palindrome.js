d42bbda516d6206e847466fad592c4d5b22a54f4
Artem Aksentenko MAC <html@centrsvet.ru>
2021-11-20 11:57:12 +0300

    pallindome

--- problems/008-palindrome.js
+++ problems/008-palindrome.js
@@ -13,7 +13,19 @@
  * @returns {boolean}
  */
 function isPalindrome(value) {
-    return undefined;
+    const string = value.split("");
+    let i = 0;
+    let j = string.length - 1;
+
+    while (i < j) {
+        if (string[i] !== string[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+
+    return true;
 }
 
 module.exports = isPalindrome;
